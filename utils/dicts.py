2377a3681d172849dc5b41f00ff298cebfdc90f6
Михаил Шикульский <shikul_m@mail.ru>
2023-04-07 23:38:57 +0400

    Add dicts.py

---
+++ utils/dicts.py
@@ -0,0 +1,11 @@
+def get_val(collection, key, default='git'):
+    """
+    Функция возвращает значение из словаря по переданному ключу, если ключ существует. В ином случае возвращается значение
+default
+    :param collection: словарь
+    :param key:  ключ
+    :param default: значение по умолчанию
+    :return: 
+    """
+    pass
+
